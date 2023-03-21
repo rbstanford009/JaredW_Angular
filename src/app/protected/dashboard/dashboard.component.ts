b9a2408f804ab656b56737e0ad9ad1316cf07dc1
ME <118782040+rbstanford009@users.noreply.github.com>
2023-03-21 12:17:56 -0600

    search works with string

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,22 +1,89 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { LOCALSTORAGE_TOKEN_KEY } from 'src/app/app.module';
+import {Observable} from "rxjs";
+import {HttpService} from "../../http.service";
+import {HttpClient} from "@angular/common/http";
+import {map} from "rxjs/operators";
+import {Movie} from "../../moviesonline/movie";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  private moviesUrl = 'api/movies';  // URL to web api      // https://api.themoviedb.org/3/search/movie?api_key=083f0465f131ae121114d5e51a6d4ddf&language=en-US&query=the&page=1&include_adult=true
+
+  public movies: Movie[] = [];
   constructor(
+    private httpService: HttpService,
+    private httpClient: HttpClient,
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.getMovies();
+    //   this.getMoviesMany();
+    /*
+
+    */
+  }
   logout() {
     // Removes the jwt token from the local storage, so the user gets logged out & then navigate back to the "public" routes
     localStorage.removeItem(LOCALSTORAGE_TOKEN_KEY);
     this.router.navigate(['../../']);
   }
 
+  getGenres(): Observable<any> {
+
+    return this.httpService.getGenres();
+  }
+
+  getMovies(): void {
+    console.log(this.movies);
+
+    this.httpService.searchMovie("Beast")
+      .pipe(
+        map(data => Object.keys(data).map(key => data[key]))
+      )
+      .subscribe((movies: Movie[]) => this.movies = movies);
+
+    console.log(this.movies);
+
+    console.log('----------PRE-------------');
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=083f0465f131ae121114d5e51a6d4ddf&language=en-US&query=the&page=1&include_adult=true';
+    this.httpClient.get(url).subscribe(console.log);
+    let simpleTest = this.httpClient.get(url); //.subscribe(console.log);
+
+    console.log(simpleTest);
+
+
+  }
+
+
+
+  searchMovie(name: string): Movie[] {
+
+
+    console.log(this.movies);
+
+    this.httpService.searchMovie(name)
+      .pipe(
+        map(data => Object.keys(data).map(key => data[key]))
+      )
+      .subscribe((movies: Movie[]) => this.movies = movies);
+
+    console.log(this.movies);
+
+    console.log('----------PRE-------------');
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=083f0465f131ae121114d5e51a6d4ddf&language=en-US&query=the&page=1&include_adult=true';
+    this.httpClient.get(url).subscribe(console.log);
+    let simpleTest = this.httpClient.get(url); //.subscribe(console.log);
+
+    console.log(simpleTest);
+    //   test.pipe().
+    return this.movies
+  }
 }
